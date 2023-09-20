c9d31feeea79117d98239b872abbd92ef7ee141d
Theo Lee <theo.leehao@elev.ga.ntig.se>
2023-09-20 11:02:00 +0200

    Number generator

--- Fighting game/Program.cs
+++ Fighting game/Program.cs
@@ -1,7 +1,26 @@
-﻿
+﻿Console.WriteLine("P1 enter your name");
+string p1 = Console.ReadLine();
+
+// Console.WriteLine("P2 enter your name");
+// string p2 = Console.ReadLine();
+
+int playerhp = 50;
+int enemyhp = 50;
+
 Random generator = new Random();
+int tal = generator.Next(1,21);
 
-int tal = generator.Next();
+Random gen = new Random();
+int dmg = gen.Next(1,21);
 
-Console.WriteLine("");
+
+while (playerhp > 0 && enemyhp > 0)
+{
+ playerhp-=tal;
+ enemyhp-=dmg;   
+}
+
+
+
+Console.WriteLine("Press enter");
 Console.ReadLine();
